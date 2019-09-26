97cc99e0ed96e1fe8b47ce08de3cedc12d690cd1
leijf <leijinfeng@geetest.com>
2019-09-26 15:36:49 +0800

    change minSdkVersion 19 to 16

--- demo/src/main/java/com/lxzh123/sdkshelldemo/DemoApp.java
+++ demo/src/main/java/com/lxzh123/sdkshelldemo/DemoApp.java
@@ -2,14 +2,17 @@ package com.lxzh123.sdkshelldemo;
 
 import android.app.Application;
 
-import com.lxzh123.libshell.Shell;
+import com.lxzh123.injector.Loader;
+
+//import com.lxzh123.libshell.Shell;
 
 public class DemoApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         try{
-            Shell.get().init(this);
+//            Shell.get().init(this);
+            Loader.init(this);
         }catch (Exception ex) {
             ex.printStackTrace();
         }
