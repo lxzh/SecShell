6dffc24f9662bab64a7c3030fc45dad3e7f910d2
ljf1239848066 <1239848066@qq.com>
2019-10-09 01:49:41 +0800

    bugfix for build.gradle of automator

--- libsag/src/main/java/com/lxzh123/libsag/Main.java
+++ libsag/src/main/java/com/lxzh123/libsag/Main.java
@@ -10,11 +10,12 @@ import java.net.URLClassLoader;
  * date        2019-09-28
  */
 class Main {
-    private final static String OUTPUT_FOLDER = "D:\\Android\\Code\\SecShell\\corestub\\src\\main\\java";
-//    private final static String OUTPUT_FOLDER = "/Users/a1239848066/Develop/AS/SecShell/corestub/src/main/java";
+    //private final static String OUTPUT_FOLDER = "D:\\Android\\Code\\SecShell\\corestub\\src\\main\\java";
+    private final static String OUTPUT_FOLDER = "/Users/a1239848066/Develop/AS/SecShell/corestub/src/main/java";
 
     public static void main(String[] args) {
-        args= new String[]{"D:\\Android\\Code\\SecShell\\libsag\\output\\libcore.jar"};
+        System.out.println("PROJECT_ROOT_FOLDER:" + System.getProperty("PROJECT_ROOT_FOLDER"));
+//        args= new String[]{"D:\\Android\\Code\\SecShell\\libsag\\output\\libcore.jar"};
 //        args= new String[]{"/Users/a1239848066/Develop/AS/SecShell/libsag/output/libcore.jar"};
         if (args.length < 1) {
             System.out.println("add a filepath to extract api");
@@ -29,6 +30,9 @@ class Main {
         System.out.println(file.toString() + " " + file.getAbsolutePath());
 //        String folder = file.getAbsoluteFile().getParent();
         String folder = OUTPUT_FOLDER;
+        if (args.length == 2) {
+            folder = args[1];
+        }
         URL url = null;
         try {
             url = new URL("file:" + filepath);
